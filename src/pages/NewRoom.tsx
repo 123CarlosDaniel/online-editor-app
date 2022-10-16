6491fd656a20fe50dee1c94896a51d9ede4c8d8d
carlosdcm <carloschoqque123@gmail.com>
2022-10-15 19:52:57 -0500

    updated newRoom interface

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,16 +1,30 @@
+import { FormEventHandler } from "react";
 import { Link } from "react-router-dom";
 import LogoutButton from "../components/LogoutButton";
 
 export default function NewRoom() {
-  
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault()
+    
+  }
   return (
     <div className="Layout">
       <div className="button_container">
         <Link className="button" to={'/'}>
           Principal
         </Link>
-        <Link className="button" to={'/newRoom'}>Create New Room</Link>
+        <Link className="button" to={'/panel'}>
+          Panel
+        </Link>
         <LogoutButton/>
+      </div>
+      <div>
+        <h2>Create Room</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" />
+          <button type="submit">Create</button>
+        </form>
       </div>
     </div>
   )
